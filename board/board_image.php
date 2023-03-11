4bc35e8ae1163b51c4d705cbee1c7c6131d07841
alsendrha <alsendrha@naver.com>
2023-03-11 22:56:03 +0900

    이미지 업로드

--- board/board_image.php
+++ board/board_image.php
@@ -1,9 +1,7 @@
 <?php
 include '../connection.php';
 
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image'])) {
-
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $boardTitle = $_POST["board_title"];
     $boardWriter = $_POST["board_writer"];
     $boardContent = $_POST["board_content"];
@@ -12,34 +10,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image'])) {
     if (isset($_FILES['image'])) {
         $file_error = $_FILES['image']['error'];
         if ($file_error === 0) {
-            // 파일 업로드 코드
+            $file_name = $_FILES['image']['name'];
+            $file_temp = $_FILES['image']['tmp_name'];
+            $file_size = $_FILES['image']['size'];
+            $image_path = 'uploads/' . $file_name;
+            move_uploaded_file($file_temp, $image_path);
         } else {
             echo json_encode(array("success" => false));
+            exit();
         }
-    } else {
-        echo json_encode(array("success" => false, "message" => "No file uploaded"));
-        exit();
     }
 
-    $file_error = $_FILES['image']['error'];
-    if ($file_error === 0) {
-        $file_name = $_FILES['image']['name'];
-        $file_temp = $_FILES['image']['tmp_name'];
-        $file_size = $_FILES['image']['size'];
+    $sqlQuery = "INSERT INTO user_board SET board_title = '$boardTitle', board_writer = '$boardWriter',
+                board_content = '$boardContent'";
+    if (isset($image_path)) {
+        $sqlQuery .= ", image_path = '$image_path'";
+    }
+    $sqlQuery .= ", date = NOW()";
 
+    mysqli_query($connection, $sqlQuery);
 
-        $image_path = 'uploads/' . $file_name;
-        move_uploaded_file($file_temp, $image_path);
-
-        $sqlQuery = "INSERT INTO user_board SET board_title = '$boardTitle', board_writer = '$boardWriter',
-                    board_content = '$boardContent', image_path = '$image_path', date = NOW()";
-        mysqli_query($connection, $sqlQuery);
-
-        if (mysqli_affected_rows($connection) > 0) {
-            echo json_encode(array("success" => true));
-        } else {
-            echo json_encode(array("success" => false));
-        }
+    if (mysqli_affected_rows($connection) > 0) {
+        echo json_encode(array("success" => true));
     } else {
         echo json_encode(array("success" => false));
     }
